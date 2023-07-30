f5f9992b7d321fcee58fe6aa17cd396c892eb7f9
Linh <linhtkph13315@fpt.edu.vn>
2023-07-30 10:58:22 +0700

    asm1

---
+++ asm1/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Homepage, DetailPage, Dashboard, ProductPage, AddProduct, UpdateProductPage } from './pages'
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+  }, [])
+
+  const removeProduct = (id) => {
+    fetch(`http://localhost:3000/products/${id}`, {
+      method: "DELETE"
+    }).then(() => setProducts(products.filter((item) => item.id != id)))
+  }
+  
+  const addProduct = (product) => {
+    fetch(`http://localhost:3000/products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+  }
+  const onUpdate = (product) => {
+    fetch(`http://localhost:3000/products/${product.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+  }
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage products={products} />} />
+        <Route path='/detail/:id' element={<DetailPage products={products} />} />
+        <Route path='/admin' element={<Dashboard />} />
+        <Route path='/admin/product' element={<ProductPage products={products} removeProduct={removeProduct} />} />
+        <Route path='/admin/product/add' element={<AddProduct addProduct={addProduct} />} />
+        <Route path='/admin/product/update/:id' element={<UpdateProductPage onUpdate={onUpdate} products={products} />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ asm1/src/pages/Admin/AddProduct.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react';
+
+const AddProductPage = ({ addProduct }) => {
+    const [data, setData] = useState({})
+    const onHandleChange = (event) => {
+        const { name, value } = event.target; 
+        const newData = { ...data, [name]: value } 
+        setData(newData); 
+    }
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        addProduct(data); 
+    }
+
+    return (
+        <div>
+            <form action="" onSubmit={onHandleSubmit}>
+                <input type="text" placeholder='Enter Product Name' onChange={onHandleChange} name='name' />
+                <input type="text" placeholder='Enter Product Price' onChange={onHandleChange} name='price' />
+                <button>Add New</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddProductPage
